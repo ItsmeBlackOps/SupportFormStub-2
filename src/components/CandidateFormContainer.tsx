0118ffb5dc47099b7127cabdb7cd4bd0ff9aaa9b
ItsmeBlackOps <109592102+ItsmeBlackOps@users.noreply.github.com>
2025-05-17 21:29:07 -0400

    Update autocomplete to handle Expert value

--- src/components/CandidateFormContainer.tsx
+++ src/components/CandidateFormContainer.tsx
@@ -123,7 +123,7 @@ export default function CandidateFormContainer({
       remarks: '',
       duration: '60',
       screeningDone: false,
-      expert: formData.Expert,
+      expert: formData.expert,
     });
     setErrors({});
   };
